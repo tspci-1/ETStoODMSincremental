ba181ecd456c67aaa254f4be48d6717696ef2c73
simmons.tim <simmons.tim@scsmail.net>
2018-04-07 21:11:35 -0500

    Don't want two .cs files with same name as Program.

--- FEquery/ProgramForm.cs
+++ FEquery/ProgramForm.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace FEquery
 {
-    static class Program
+    static class ProgramForm
     {
         /// <summary>
         /// The main entry point for the application.
